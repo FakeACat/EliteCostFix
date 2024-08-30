e7035cbc9f96cc392a266dc00f0dbe61a79ebec7
FakeACat <sam.byford@outlook.com>
2024-08-30 14:59:19 +0100

    created

---
+++ EliteCostFix/EliteCostFix.cs
@@ -0,0 +1,69 @@
+using System.Linq;
+using BepInEx;
+using RoR2;
+
+namespace EliteCostFix
+{
+    [BepInPlugin(PluginGUID, PluginName, PluginVersion)]
+    public class EliteCostFix : BaseUnityPlugin
+    {
+        public const string PluginGUID = PluginAuthor + "." + PluginName;
+        public const string PluginAuthor = "acats";
+        public const string PluginName = "EliteCostFix";
+        public const string PluginVersion = "1.0.0";
+
+        public void Awake()
+        {
+            Log.Init(Logger);
+
+            On.RoR2.CombatDirector.Init += orig =>
+            {
+                orig();
+                CombatDirector.EliteTierDef normalTier1 = null;
+                CombatDirector.EliteTierDef tier1WithGold = null;
+
+                // find tier with overloading and no gilded (should be normal tier 1)
+                foreach (var tier in CombatDirector.eliteTiers)
+                {
+                    if (
+                        tier.eliteTypes.Contains(RoR2Content.Elites.Lightning)
+                        && !tier.eliteTypes.Contains(DLC2Content.Elites.Aurelionite)
+                    )
+                    {
+                        normalTier1 = tier;
+                        break;
+                    }
+                }
+                // find tier with gilded (should be the new unintentionally cheaper tier 1)
+                foreach (var tier in CombatDirector.eliteTiers)
+                {
+                    if (tier.eliteTypes.Contains(DLC2Content.Elites.Aurelionite))
+                    {
+                        tier1WithGold = tier;
+                        break;
+                    }
+                }
+
+                if (normalTier1 == null)
+                {
+                    Log.Error("Unable to find elite tier 1!");
+                    return;
+                }
+
+                if (tier1WithGold == null)
+                {
+                    Log.Error("Unable to find gilded elite tier!");
+                    return;
+                }
+
+                // fix the cost
+                tier1WithGold.costMultiplier = normalTier1.costMultiplier;
+
+                // disable normal tier 1 when the new tier becomes available
+                var vanillaIsAvailable = normalTier1.isAvailable;
+                normalTier1.isAvailable = rules =>
+                    vanillaIsAvailable(rules) && !tier1WithGold.isAvailable(rules);
+            };
+        }
+    }
+}
---
+++ README.md
@@ -0,0 +1,13 @@
+Quick fix for elite spam in the new update
+
+In the new update, a new elite tier was introduced. It contains all the normal tier 1 elites and the new gilded elites, and becomes available once you've cleared 2 stages.
+
+The problem is that this new elite tier has a cost multiplier of 3.5x (compared to the normal tier 1 cost multiplier of 6x). This means that as soon as this tier is available, all tier 1 elites can be spawned for only slightly more than half of their intended cost.
+
+This mod attempts to fix the issue by:
+-Finding the normal tier 1 by looking for whichever tier contains overloading elites but not gilded elites
+-Finding the new tier by looking for whichever tier contains gilded elites
+-Setting the new tier's cost multiplier to the same as the normal tier 1's cost multiplier
+-Disabling the normal tier 1 once the new tier is available (not sure if this is actually needed)
+
+This achieves what I believe was the intended behaviour - introducing gilded elites to the tier 1 elite pool starting at stage 3.
